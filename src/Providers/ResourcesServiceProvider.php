a2c61fb6f1646686ffaa133e4c15e379c0983859
Allen Li <ali@goodcatch.cn>
2021-09-22 15:29:55 +0800

    修改发布路径

--- src/Providers/ResourcesServiceProvider.php
+++ src/Providers/ResourcesServiceProvider.php
@@ -33,7 +33,7 @@ class ResourcesServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole ()) {
             $src = __DIR__ . '/../..';
             $this->publishes ([
-                $src . '/public/dist' =>  public_path('dist/module-base')
+                $src . '/public/dist' =>  public_path('dist')
             ], 'laravel-modules');
         }
     }
